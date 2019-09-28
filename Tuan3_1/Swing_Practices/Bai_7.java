3eba38c0e8fae147dea4be28a4506033d5710de8
Trần Nguyên Vẹn <49260909+nguyenven299@users.noreply.github.com>
2019-09-28 19:03:53 +0700

    Add files via upload

---
+++ Tuan3_1/Swing_Practices/Bai_7.java
@@ -0,0 +1,12 @@
+package Swing_Practices;
+
+import javax.swing.JFrame;
+
+public class Bai_7 extends JFrame{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
